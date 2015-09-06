d2fdd13459a73a8cee6d0155fadbac148d4dc2ca
Raffaele Salmaso <raffaele@salmaso.org>
2015-09-06 08:18:58 +0200

    import project

---
+++ README.md
@@ -0,0 +1,11 @@
+Various scripts to ease django development
+
+dj
+==
+
+Runs ./manage.py from anywehere in the django project
+
+rs
+--
+
+Runs (by default) ./manage.py runserver 0.0.0.0:8000 from anywhere in the django project
---
+++ setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2007-2015, Raffaele Salmaso <raffaele@salmaso.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import io
+import os
+from setuptools import setup
+
+__author__ = "Raffaele Salmaso",
+__author_mail__ = "raffaele@salmaso.org",
+__version__ = "0.1.0a0"
+
+setup(
+    packages=[],
+    name="django-scripts",
+    version=__version__,
+    description = io.open(os.path.join(os.path.dirname(__file__), "README.md"), "rU").read(),
+    long_description="",
+    author=__author__,
+    author_email=__author_mail__,
+    url="https://bitbucket.org/rsalmaso/django-scripts",
+    license="MIT License",
+    classifiers=["License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Utilities",
+        "Development Status :: 4 - Beta",
+    ],
+    scripts=[
+        "bin/dj",
+        "bin/dj2",
+        "bin/dj3",
+        "bin/rs",
+        "bin/rs2",
+        "bin/rs3",
+    ],
+    include_package_data=True,
+    install_requires=[],
+    zip_safe=False,
+)

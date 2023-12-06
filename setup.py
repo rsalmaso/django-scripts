607da5cf79912f76784d42ef673f5471a14d255b
Raffaele Salmaso <raffaele@salmaso.org>
2023-12-06 18:47:34 +0100

    Convert setup configuration to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # Copyright (C) Raffaele Salmaso <raffaele@salmaso.org>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -20,39 +18,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import io
-import os
-
 from setuptools import setup
 
-import django_scripts
-
-setup(
-    packages=["django_scripts"],
-    name="django-scripts",
-    version=django_scripts.__version__,
-    description=io.open(os.path.join(os.path.dirname(__file__), "README.md"), "rt").read(),
-    long_description="",
-    author=django_scripts.__author__,
-    author_email=django_scripts.__author_email__,
-    url="https://bitbucket.org/rsalmaso/django-scripts",
-    license="MIT License",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Topic :: Utilities",
-        "Development Status :: 4 - Beta",
-    ],
-    entry_points={"console_scripts": ["dj = django_scripts.dj:main"]},
-    include_package_data=True,
-    install_requires=["stua"],
-    zip_safe=False,
-    python_requires=">=3",
-)
+setup()
